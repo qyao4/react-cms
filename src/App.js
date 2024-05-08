8c6d3a72dd47ccffee98389db0443e8b978f0939
qyao4 <138896505+qyao4@users.noreply.github.com>
2024-05-07 19:45:24 -0500

    lazy router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import Login from '@/pages/login'
-import MainLayout from '@/pages/layout'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
+
+// 路由懒加载
+import { lazy, Suspense } from 'react'
+// import Login from '@/pages/login'
+// import MainLayout from '@/pages/layout'
+// import Home from '@/pages/Home'
+// import Article from '@/pages/Article'
+// import Publish from '@/pages/Publish'
+
 
 import { AuthComponent } from '@/components/AuthComponent'
 import './App.css'
 import { setNavigate } from '@/utils'
+
+const Login = lazy(() => import('@/pages/login'))
+const MainLayout = lazy(() => import('@/pages/layout'))
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
+
 // import { Button, Space, DatePicker, version } from 'antd'
 
 // 这里定义的SetNavigator组件一定要是BrowserRouter的子组建
@@ -24,15 +35,28 @@ function App () {
     <BrowserRouter>
       <SetNavigator />
       <div className="App">
-        <Routes>
-          {/* Layout需要鉴权处理 */}
-          <Route path='/' element={<AuthComponent><MainLayout /></AuthComponent>}>
-            <Route index element={<Home />}></Route>
-            <Route path='/article' element={<Article />}></Route>
-            <Route path='/publish' element={<Publish />}></Route>
-          </Route>
-          <Route path='/login' element={<Login />}></Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 200
+              }}
+            >
+              loading...
+            </div>
+          }
+        >
+          <Routes>
+            {/* Layout需要鉴权处理 */}
+            <Route path='/' element={<AuthComponent><MainLayout /></AuthComponent>}>
+              <Route index element={<Home />}></Route>
+              <Route path='/article' element={<Article />}></Route>
+              <Route path='/publish' element={<Publish />}></Route>
+            </Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
 
